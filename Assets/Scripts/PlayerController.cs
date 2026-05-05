3da125c84abd0e789ff8fad7f33f3f7677a6dfe6
AfeefAhmer <s2401102@edu.bc.fi>
2026-05-05 12:25:21 +0300

    tehtävä 6 toinen kenttä

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"); 
+        float moveY = Input.GetAxis("Vertical");   
+
+        Vector3 movement = new Vector3(moveX, moveY, 0f);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerDataManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerDataManager : MonoBehaviour
+{
+    public static PlayerDataManager Instance { get; private set; }
+
+    [SerializeField] private int money = 100;
+    [SerializeField] private TMP_Text coinText;
+
+    public int Money => money;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (coinText != null)
+            coinText.text = "Coins: " + money;
+    }
+
+    public int AddMoney(int amount)
+    {
+        if (amount <= 0) return money;
+
+        money += amount;
+        UpdateUI();
+        return money;
+    }
+
+    public bool TakeMoney(int amount)
+    {
+        if (amount <= 0) return true;
+        if (money < amount) return false;
+
+        money -= amount;
+        UpdateUI();
+        return true;
+    }
+}
